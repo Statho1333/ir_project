4326575358e8f53925cae21a29640f76069f682c
tsitlakg <giorg@DESKTOP-B66CO54.>
2025-12-07 23:03:09 +0200

    api testing

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def main():
-    print("Hello from ir-project!")
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
+app = FastAPI()
+templates = Jinja2Templates(directory="frontend/templates")
+app.mount("/static", StaticFiles(directory="frontend/static"), name="static")
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
