53b8f8952f450c3c033e01b92748039c6e0ce5e4
Statho1333 <istathop@csd.auth.gr>
2025-12-18 12:39:19 +0200

    before merging

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Union
-
+from core.search_engine import Search_Engine
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -13,3 +13,14 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+def main():
+    engine = Search_Engine()
+    print("Search Engine is ready to use.")
+
+    results = engine.search("φορολογία επιχειρήσεων", top_k=5)
+    for r in results:
+        print(r)
+
+if __name__ == "__main__":
+    main()
